31be5f2d136936a0aa319e246312333915ef80f7
usmsk <usmsk@student.kit.edu>
2022-02-14 22:12:02 +0100

    Initial commit

---
+++ src/ip/IP.java
@@ -0,0 +1,85 @@
+package src.ip;
+
+import src.exceptions.ParseException;
+
+/**
+ * class to provide functionality regarding IP-Addresses
+ *
+ * @author usmsk
+ * @version 1.0
+ */
+public class IP implements Comparable<IP> {
+    /**
+     * regex stating correct IP-Address format
+     */
+    public static final String IP_PATTERN = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])"
+            + "(\\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){3})$";
+
+    /**
+     * contains binary interpretation of IP-Address
+     */
+    public final String ipAsBinary;
+    public int ip;
+
+    /**
+     * constructor for IPs.
+     * checks if specified IP matches IP_PATTERN.
+     * then builds ipAsBinary by separately interpreting the 4 sections as 8bit binary numbers and concatenating them.
+     *
+     * @param pointNotation Ip address in point notation
+     * @throws ParseException if ipAddress does not match proper point Notation(IP_PATTERN)
+     */
+    public IP(final String pointNotation) throws ParseException {
+        if ((!pointNotation.matches(IP_PATTERN))) {
+            throw new ParseException("This IP-Address is not valid. It does not match the required pattern");
+        }
+
+        String rest = pointNotation;
+        String temp;
+        StringBuilder ipAsBinary = new StringBuilder();
+        int indexOfFirstPoint;
+        for (int i = 0; i < 4; i++) {
+            indexOfFirstPoint = rest.indexOf('.');
+            temp = rest.substring(0, indexOfFirstPoint);  //interpretation as 8bit binary and concatenation
+            ipAsBinary.append(String.format("%8s", Integer.toBinaryString(Integer.parseInt(temp)))
+                    .replace(' ', '0'));
+            if (i < 2) {
+                rest = rest.substring(++indexOfFirstPoint);
+            }
+        }
+        this.ipAsBinary = ipAsBinary.toString();
+    }
+
+    @Override
+    public String toString() {
+        String temp = ipAsBinary;
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            output.append(Integer.parseInt(temp.substring(0, 8), 2));
+            if (i < 3) {
+                output.append('.');
+                temp = temp.substring(8);
+            }
+            
+        }
+        return output.toString();
+    }
+
+    @Override
+    public int compareTo(IP o) {
+        if (Long.parseLong(o.ipAsBinary, 2) > Long.parseLong(this.ipAsBinary, 2)) {
+            return -1;
+        } else if (Long.parseLong(o.ipAsBinary, 2) < Long.parseLong(this.ipAsBinary, 2)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o.getClass() == IP.class) {
+            return compareTo((IP) o) == 0;
+        }
+        return false;
+    }
+}
---
+++ src/ip/IPTest.java
@@ -0,0 +1,56 @@
+package src.ip;
+
+import src.exceptions.ParseException;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class IPTest {
+    IP ip1 = new IP("0.0.0.0");
+    IP ip11 = new IP("0.0.0.0");
+    IP ip2 = new IP("211.1.1.1");
+    IP ip22 = new IP("211.1.1.1");
+    IP ip3 = new IP("255.255.255.255");
+    IP ip33 = new IP("255.255.255.255");
+
+    public IPTest() throws ParseException {
+    }
+
+    @Test(expected = ParseException.class)
+    public void errorTest() throws ParseException {
+        IP differentIP = new IP("2111.1.1.1");
+    }
+
+    @Test
+    public void constructorTest() throws ParseException {
+        assertEquals("11010011000000010000000100000001", ip2.ipAsBinary);
+    }
+
+    @Test
+    public void comparisonTest() throws ParseException {
+        assertEquals(-1, ip1.compareTo(ip2));
+        assertEquals(-1, ip1.compareTo(ip3));
+        assertEquals(0, ip1.compareTo(ip11));
+        assertEquals(1, ip2.compareTo(ip1));
+        assertEquals(-1, ip2.compareTo(ip3));
+        assertEquals(0, ip2.compareTo(ip22));
+        assertEquals(1, ip3.compareTo(ip1));
+        assertEquals(1, ip3.compareTo(ip2));
+        assertEquals(0, ip3.compareTo(ip33));
+    }
+
+    @Test
+    public void equalsTest() throws ParseException {
+        Object o = new Object();
+        assertEquals(ip2, ip22);
+        assertNotEquals(ip2, ip3);
+        assertNotEquals(ip2, o);
+    }
+
+    @Test
+    public void toStringTest() throws ParseException {
+        assertEquals("0.0.0.0", ip1.toString());
+        assertEquals("211.1.1.1", ip2.toString());
+        assertEquals("255.255.255.255", ip3.toString());
+    }
+}
